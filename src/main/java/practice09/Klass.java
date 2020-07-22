eeb6bbd4c69541477fe9aef2c244171ba2aa224f
chenmianhan <mianhan1998@139.com>
2020-07-23 00:26:04 +0800

    [James Chen] feature :update the pratice 09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,29 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public int getNumber() {
+        return number;
+    }
+    public Klass(int number){
+        this.number=number;
+    }
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student leader) {
+        if(leader.getKlass()==null||!leader.getKlass().equals(this))
+            System.out.println("It is not one of us.");
+        else this.leader = leader;
+    }
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
