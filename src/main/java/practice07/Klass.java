4806a4a817ce459b14244f3f6b0984a976e01c62
JAMES M H CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENJA28@OOCL.COM>
2020-07-22 16:35:13 +0800

    [James Chen] feature : add the Class Teacher, Student, Teacher in the pratice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,15 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+    public Klass(int number){
+        this.number=number;
+    }
+    public String getDisplayName(){
+        return "Class "+number;
+    }
 }
