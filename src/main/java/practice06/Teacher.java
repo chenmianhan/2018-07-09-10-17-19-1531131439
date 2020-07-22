61e6544f7abc20a5a58fcc519f3f1ae810cec7f5
JAMES M H CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENJA28@OOCL.COM>
2020-07-22 16:20:39 +0800

    [James Chen] feature : add the Class Teacher, Student, Teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int  klass;
+    public Teacher(String name,int age ,int klass){
+        super(name, age);
+        this.klass=klass;
+    }
+    public Teacher(String name,int age){
+        super(name,age);
+        klass=-1;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String intro=super.introduce()+" I am a Teacher. ";
+        if(klass==-1)intro+="I teach No Class.";
+        else intro+="I teach Class "+klass+".";
+        return intro;
+    }
 }
